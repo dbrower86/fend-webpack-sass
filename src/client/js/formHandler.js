f6ff3390b99ced54e24ca6aadb3a0621eb06999e
Dan Brower <danielsbrower@gmail.com>
2020-12-22 14:48:17 -0800

    add weather

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,6 @@
+const key = ',us&appid=ce62966fa4ec5941f43072a50bb4bebc';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -7,13 +10,27 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
+//    fetch('http://localhost:8081/test')
+    getWeather(baseUrl, "91302", key)    
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        const kelvin = data.main.temp;
+        let degF =(( kelvin - 273.15) * 9/5) + 32;
+        degF = Math.trunc(degF);
+
+        document.getElementById('results').innerHTML = degF
     })
+}
+
+const getWeather = async (baseURL, zip, key)=>{
+
+    const res = await fetch(baseURL+zip+key)
+    try {
+      const data = await res.json();
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
 }
 
 export { handleSubmit }
